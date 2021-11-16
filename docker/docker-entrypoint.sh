e19c25562fe954b68ca7dcc2ae8d591105657a73
wellington <wag16098727>
2021-11-16 14:49:33 -0300

    Added docker in application (DB, Redis, Memcached, PHP, Supervisor, and Cron)

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+chmod 777 -R storage &&
+/usr/bin/supervisord &&
+service supervisor restart &&
+/etc/init.d/cron restart &&
+composer install &&
+cp .env.example .env &&
+php artisan key:generate &&
+php artisan migrate:fresh &&
+php artisan passport:install &&
+php artisan db:seed &&
+npm install &&
+#npm run dev &&
+
+echo "########################################################" &&
+echo " User: user@streamlabs.com - Senha: password            " &&
+echo "########################################################" &&
+
+php artisan serve --host=0.0.0.0 --port=8000
